bdb7899e4a310cb8a3892c3eac904df067ca7d95
keshew <artyom.korotckov2017@yandex.ru>
2025-04-28 23:28:36 +0500

    Initial Commit

---
+++ Plinxy/PlinxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlinxyApp.swift
+//  Plinxy
+//
+//  Created by Артём Коротков on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PlinxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
